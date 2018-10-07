3fdf77ed67b9128dd38d829d5555320368624c47
lucasll2 <xx-luki-xx@hotmail.com>
2018-10-07 17:48:41 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const cfg require('./index.json'); // a garder en version desktop
+const token = process.env.token // a garder en version heroku
+const prefix = ("?");
+
+bot.on('ready', function () {
+    console.log("Je suis prêt à être utilisé.")
+    bot.user.setActivity('rien').catch(console.error)
+});
+
+bot.on('guildMemberAdd', member => {
+    member.createDM().then(channel => {
+        return channel.send('Bienvenue le discord de la team Beast-gang' + member.displayName)
+        console.log(`${member.displayName} à rejoind le serveur.`)
+    }).catch(console.error)
+});
+
+const ban = require('./kick et ban/ban');
+
+
+bot.on('message', function (message){
+    if (ban.match(message)){
+        return ban.action(message)
+    }
+});
+
+
+bot.on('message', msg => {
+    if (msg.content === "bonjour"){
+        msg.reply("Heureux de te revoir parmis nous.")
+    }
+    if (msg.content.match(/salut/i)) {
+            msg.reply('Je suis d\'accord avec toi.')
+    }
+    if (msg.content === prefix + "site"){
+        msg.channel.send("http://beast-gang.fr/")
+        console.log("Une personne a demandé pour aller sur le site de la team.")
+    }
+
+});
+
+bot.login(cfg.token); //a garder en version desktop
+bot.login(token); //a garder en version heroku
